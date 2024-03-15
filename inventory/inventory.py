e60714329d49adaf43bef718dbddc73a4a0c0f22
JesseAger <agerjesse6@gmail.com>
2024-03-16 02:21:40 +0300

    input_variables

--- inventory/inventory.py
+++ inventory/inventory.py
@@ -1,20 +1,22 @@
 import os
 import datetime
 import time
-from time_date import Time, Date
+import time_date
+Date = time_date.Date()
+Time = time_date.Time()
 
-def save_items_to_inventory(SerialNo: int, Item: str, Price: float, Date_of_execution = Date, Time_of_execution = Time):
-        if not os.path.exists('Inventory.csv'):
-            with open('Inventory.csv', 'w') as file:
-                file.write("SerialNo\tItem\tPrice\tDate_of_execution\tTime_of_execution")
-        with open('Inventory.csv', 'a') as file:
-            file.write(f"{SerialNo.zfill(4)}\t{Item}\t{Price}\t{Date_of_execution}\t{Time_of_execution}\n")
+def save_items_to_inventory(SerialNo: int, Item: str, Price: float, date = Date, time = Time):
+        if not os.path.exists('Inventory.sql'):
+            with open('Inventory.sql', 'w') as file:
+                file.write("SerialNo\tItem\tPrice\tdate\t\ttime")
+        with open('Inventory.sql', 'a') as file:
+            file.write(f"{SerialNo.zfill(4)}\t{Item}\t{Price}\t{date}\t{time}\n")
 
 
 def read_items_from_inventory():
     Items = []
-    if os.path.exists('Inventory.csv'):
-        with open('Inventory.csv') as file:
+    if os.path.exists('Inventory.sql'):
+        with open('Inventory.sql') as file:
             Items= file.readlines
     # return [line.strip().split('\t') for line in Items]
 
@@ -26,7 +28,7 @@ def main():
         if Item_input == 'q':
             break
 
-        Items = Item_input.split(',')
+        Items = Item_input.split('\t')
         if len(Items) !=3:
             print('Incomplete Inventory information')
             continue
--- inventory/time_date.py
+++ inventory/time_date.py
@@ -2,15 +2,14 @@ import time
 
 def Date():
     current_date= time.localtime()
-    exact_date = time.strftime("%d/%m/%Y", current_date)    
+    exact_date = time.strftime("%d/%m/%y", current_date)    
     return exact_date
 Date()
 
 def Time():
     current_time = time.localtime()
     exact_time = time.strftime("%I:%M:%S %p", current_time)
-    return(exact_time)
-
+    return exact_time
 
 Time()
 

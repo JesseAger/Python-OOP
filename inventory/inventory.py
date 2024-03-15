ffcfd01c3c705b7f51c572eb99abc6062f455f05
JesseAger <agerjesse6@gmail.com>
2024-03-16 01:11:15 +0300

    test_it

--- inventory/inventory.py
+++ inventory/inventory.py
@@ -1,13 +1,14 @@
 import os
-from datetime import datetime
+import datetime
+import time
 from time_date import Time, Date
 
-def save_items_to_inventory(SerialNo: int, Item: str, Price: float, Date = Date, Time=Time):
+def save_items_to_inventory(SerialNo: int, Item: str, Price: float, Date_of_execution = Date, Time_of_execution = Time):
         if not os.path.exists('Inventory.csv'):
             with open('Inventory.csv', 'w') as file:
-                file.write("SerialNo\tItem\tPrice\tDate\tTime")
+                file.write("SerialNo\tItem\tPrice\tDate_of_execution\tTime_of_execution")
         with open('Inventory.csv', 'a') as file:
-            file.write(f"{SerialNo.zfill(4)}\t{Item}\t{Price}\t{Date}\t{Time}\n")
+            file.write(f"{SerialNo.zfill(4)}\t{Item}\t{Price}\t{Date_of_execution}\t{Time_of_execution}\n")
 
 
 def read_items_from_inventory():
@@ -26,12 +27,12 @@ def main():
             break
 
         Items = Item_input.split(',')
-        if len(Items) !=4:
+        if len(Items) !=3:
             print('Incomplete Inventory information')
             continue
 
         SerialNo, Item, Price = [components.strip() for components in Items]
-        save_items_to_inventory(SerialNo, Item, Price, Date)
+        save_items_to_inventory(SerialNo, Item, Price, Date, Time)
         print('Inventory Added')
 
         added_item = read_items_from_inventory()
